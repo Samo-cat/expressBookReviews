8be6e618999102b4891505e8c3ce14f9ec0efefb
Samo-cat <samosational@gmail.com>
2025-02-12 21:06:31 -0500

    expressbookreviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) {
+        users.push({"username":username, "password":password});
+        return res.status(200).json({message: "Customer successfully registered, Now you can login"});
+    } else {
+        return res.status(404).json({message: "Customer with same username already exists!"});
+    }
+    }
+    return res.status(404).json({message: "Unable to register customer"})
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
